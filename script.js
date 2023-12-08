235f00055b8fc6d57233107659bfbd5b5dd4952b
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-08 11:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+// Examples
+functionName(); // Alerts "functionName"
+
+// Another example with a different function name
+function AccioJob() {
+    alert(arguments.callee.name);
+}
+
+AccioJob(); // Alerts "AccioJob"
